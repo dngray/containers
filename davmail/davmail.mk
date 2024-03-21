c941646cade42ebc035302ccfc631c544fa28180
Daniel Gray <dngray@polarbear.army>
2024-03-21 05:41:53 +0000

    Switch Davmail to config

--- davmail/davmail.mk
+++ davmail/davmail.mk
@@ -10,28 +10,13 @@ davmail-build:
 .PHONY: davmail
 davmail:
 	podman run -it --replace --userns=keep-id \
-		-e DAVMAIL_SERVER=true \
-		-e DAVMAIL_ALLOWREMOTE=true \
-		-e DAVMAIL_DISABLEUPDATECHECK=true \
-		-e DAVMAIL_LOGFILEPATH=/dev/stdout \
-		-e DAVMAIL_CALDAVPORT=1080 \
-		-e DAVMAIL_IMAPPORT=1143 \
-		-e DAVMAIL_LDAPPORT=1389 \
-		-e DAVMAIL_POPPORT=1110 \
-		-e DAVMAIL_SMTPPORT=1025 \
-		-e DAVMAIL_MODE=O365Manual \
-		-e DAVMAIL_OAUTH_TOKENFILEPATH=/davmail-token \
-		-e DAVMAIL_OAUTH_CLIENTID=d3590ed6-52b3-4102-aeff-aad2292ab01c \
-		-e DAVMAIL_OAUTH_REDIRECTURI=urn:ietf:wg:oauth:2.0:oob \
-		-e JAVA_OPTS="-Xmx512M -Dsun.net.inetaddr.ttl=30n" \
-		-p 1080:1080 \
 		-p 1143:1143 \
-		-p 1389:1389 \
-		-p 1110:1110 \
 		-p 1025:1025 \
-		-v ~/.config/davmail:/davmail-token \
+		-v ~/.config/davmail/davmail_manual.properties:/davmail/davmail.properties:ro,z \
+		-v ~/.local/lib/davmail/tokens.properties:/davmail/tokens.properties:z \
+		-v ~/.local/log/davmail/davmail.log:/davmail/davmail.log:z \
 		--name davmail \
-		localhost/davmail
+		localhost/davmail /davmail/davmail.properties
 
 .PHONY: davmail-clean
 davmail-clean:
