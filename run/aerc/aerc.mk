87788e80ffe7cd4996db6b3e286d3b158fcb5809
Daniel Gray <dngray@polarbear.army>
2025-10-26 08:15:38 +0000

    aerc: switch to debian and fix selinux policy

--- run/aerc/aerc.mk
+++ run/aerc/aerc.mk
@@ -1,51 +1,62 @@
 #
-# Aerc
+# Aerc container targets
 #
+
+# Read-write mounts (local/share, local/state, Downloads, .gnupg)
+RW_MOUNTS := \
+	.local/share/mail \
+	.local/share/nvim \
+	.local/share/address-book \
+	.local/share/calendars \
+	.local/state/vdirsyncer \
+	.local/state/isync \
+	.local/share/gopass \
+	.gnupg \
+	Downloads
+
+# Read-only mounts (everything in .config + local/bin)
+RO_MOUNTS := \
+	.local/bin/msmtp-queue \
+	.local/bin/msmtpq \
+	.local/bin/sm \
+	.config/aerc \
+	.config/vdirsyncer/config \
+	.config/email-common \
+	.config/nvim \
+	.config/goimapnotify/goimapnotify.yaml \
+	.config/mailcap \
+	.config/khard \
+	.config/msmtp \
+	.config/notmuch \
+	.config/isyncrc \
+	.config/gopass/config \
+	.config/mutt
 
 .PHONY: aerc-build
 aerc-build:
+	@echo "Building Aerc container..."
 	podman build -f build/aerc/Containerfile \
-		--build-arg BUILD_DATE=$(date -u) \
-		--build-arg UID=$(UID) \
 		--build-arg LANG=en_US.UTF-8 \
+		--build-arg HOST_UID=${UID} \
+		--build-arg HOST_GID=${GID} \
 		-t aerc:latest
 
-.PHONY: aerc
 aerc:
-	podman run -it --replace --userns=keep-id \
+	@echo "Starting Aerc container..."
+	@podman run -it --replace \
+		--userns=keep-id \
+		-u ${UID}:${GID} \
 		--hostname aerc \
-		-v ~/.local/share/mail/:/home/aerc/.local/share/mail:z \
-		-v ~/.config/aerc:/home/aerc/.config/aerc:z,ro \
-		-v ~/.config/vdirsyncer/config:/home/aerc/.config/vdirsyncer/config:z,ro \
-		-v ~/.config/email-common/:/home/aerc/.config/email-common:z,ro \
-		-v ~/.config/nvim:/home/aerc/.config/nvim:z \
-		-v ~/.config/goimapnotify/goimapnotify.yaml:/home/aerc/.config/goimapnotify/goimapnotify.yaml:z,ro \
-		-v ~/.config/mailcap:/home/aerc/.config/mailcap:z,ro \
-		-v ~/.local/share/nvim:/home/aerc/.local/share/nvim:z \
-		-v ~/.local/share/address-book:/home/aerc/.local/share/address-book:z \
-		-v ~/.local/share/calendars:/home/aerc/.local/share/calendars:z \
-		-v ~/.local/state/nvim:/home/aerc/.local/state/nvim:z \
-		-v ~/.local/state/vdirsyncer:/home/aerc/.local/state/vdirsyncer:z \
-		-v ~/.gnupg:/home/aerc/.gnupg:z \
-		-v ~/.config/khard:/home/aerc/.config/khard:z,ro \
-		-v ~/.config/msmtp:/home/aerc/.config/msmtp:z,ro \
-		-v ~/.config/notmuch:/home/aerc/.config/notmuch:z,ro \
-		-v ~/.config/isyncrc:/home/aerc/.config/isyncrc:z,ro \
-		-v ~/.local/state/isync:/home/aerc/.local/state/isync:z \
-		-v ~/.config/gopass/:/home/aerc/.config/gopass/config:z,ro \
-		-v ~/.local/share/gopass/:/home/aerc/.local/share/gopass:z \
-		-v ~/.local/bin/msmtp-queue:/home/aerc/.local/bin/msmtp-queue:z,ro \
-		-v ~/.local/bin/msmtpq:/home/aerc/.local/bin/msmtpq:z,ro \
-		-v ~/.local/bin/sm:/home/aerc/.local/bin/sm:z,ro \
-		-v ~/.config/mutt:/home/aerc/.config/mutt:z \
-		-v ~/Downloads:/home/aerc/Downloads \
-		-v /run/user/${UID}:/run/user/${UID}:ro \
-		-e TZ="" \
-		--security-opt label=type:container_t \
 		--name aerc \
+		--security-opt label=type:container_t \
+		-e TZ="" \
+		$(foreach m,$(RW_MOUNTS),--mount type=bind,source=$(HOME)/$(m),target=/home/aerc/$(m) ) \
+		$(foreach m,$(RO_MOUNTS),--mount type=bind,source=$(HOME)/$(m),target=/home/aerc/$(m),readonly ) \
+		--mount type=bind,source=/run/user/${UID},target=/run/user/${UID},readonly \
 		localhost/aerc
 
 .PHONY: aerc-clean
 aerc-clean:
-	podman rm -f localhost/aerc
-	podman image rm localhost/aerc
+	@echo "Cleaning Aerc container..."
+	podman rm -f aerc || true
+	podman image rm aerc || true
--- run/aerc/container_aerc.spec
+++ run/aerc/container_aerc.spec
@@ -1,4 +1,4 @@
-Name:           containers_gpg_socket-selinux
+Name:           container_aerc-selinux
 Version:        1.0
 Release:        1.fc43
 Summary:        SELinux policy for containers and GPG socket
@@ -15,18 +15,18 @@ This SELinux policy module allows containers to access the GPG socket.
 # No preparation steps needed
 
 %build
-# Compile the .te file into the .mod file and package into a .pp.gz file
-checkmodule -M -m -o containers_gpg_socket.mod %{_sourcedir}/containers_gpg_socket.te
-semodule_package -o containers_gpg_socket.pp -m containers_gpg_socket.mod
-gzip containers_gpg_socket.pp
+# Compile the .te file into the .mod file and package into a .pp.bz2 file
+checkmodule -M -m -o container_aerc.mod %{_sourcedir}/container_aerc.te
+semodule_package -o container_aerc.pp -m container_aerc.mod
+bzip2 container_aerc.pp
 
 %install
 mkdir -p %{buildroot}%{_datadir}/selinux/packages
-install -m 644 containers_gpg_socket.pp.gz %{buildroot}%{_datadir}/selinux/packages/
+install -m 644 container_aerc.pp.bz2 %{buildroot}%{_datadir}/selinux/packages/
 
 %files
 %defattr(-,root,root,-)
-%{_datadir}/selinux/packages/containers_gpg_socket.pp.gz
+%{_datadir}/selinux/packages/container_aerc.pp.bz2
 
 %changelog
 * Tue Sep 18 2025 Daniel Gray <dngray@polarbear.army> - 1.0-1
